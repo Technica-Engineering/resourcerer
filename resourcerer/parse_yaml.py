f43b0a3503e0d6b58b94cf03cf4d1080044a4e75
Krzysztof Czarnecki <krzysztof.czarnecki@technica-engineering.de>
2022-03-22 15:48:23 +0100

    refactor: `get_yaml_obj` shouldn't default to any implicit file

--- resourcerer/parse_yaml.py
+++ resourcerer/parse_yaml.py
@@ -1,11 +1,8 @@
+from pathlib import Path
 import yaml
 
 
-def get_yaml_obj(path=None):
-    if path:
-        resources_yaml_path = path
-    else:
-        resources_yaml_path = "resources.yaml"
-    with open(resources_yaml_path, "r") as f:
-        resources = yaml.full_load(f)
-    return resources
+def get_yaml_obj(path: Path):
+    with open(path, "r") as f:
+        resources_obj = yaml.full_load(f)
+    return resources_obj

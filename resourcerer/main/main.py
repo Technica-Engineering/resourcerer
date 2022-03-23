e45d36d9aeb934a13aa473897023c66253c73710
Krzysztof Czarnecki <krzysztof.czarnecki@technica-engineering.de>
2022-03-23 10:51:47 +0100

    feat, wip: new CLI closure
    This should generate a `main` function for `get_resources` and `send_resources` CLI tool

--- resourcerer/main/main.py
+++ resourcerer/main/main.py
@@ -1,19 +1,32 @@
 from pathlib import Path
 import argparse
+from resourcerer.model import ResourcesYamlObj, CliArgs, Config
+from resourcerer.defaults import DEFAULT_FILE
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Resource retrieval for CI testing')
-    parser.add_argument('-f', '--file',
-                        type=str,
-                        help='Path to the Resources YAML file '
-                             '(default is `resources.yaml` in the working directory)'
-                        )
-    parser.add_argument('-v', '--verbosity',
-                        type=int,
-                        help="What verbosity level to set (0 - 2), default = 1", default=1)
+def make_main():
+    def closure():
+        parser = argparse.ArgumentParser(description='Resource retrieval for CI testing')
+        parser.add_argument('-f', '--file',
+                            type=str,
+                            help='Path to the Resources YAML file '
+                                 '(default is `resources.yaml` in the working directory)',
+                            default=DEFAULT_FILE)
+        parser.add_argument('-v', '--verbosity',
+                            type=int,
+                            help="What verbosity level to set (0 - 2), default = 1", default=1)
+        args = parser.parse_args()
 
+        cli_args = CliArgs(
+            Path(args.file),
+            args.verbosity
+        )
 
+        yaml_file_config = ResourcesYamlObj.from_yaml(cli_args.file)
 
-if __name__ == "__main__":
-    main()
+        config = Config(
+            cli_args,
+            yaml_file_config
+        )
+
+    return closure

77559cdee6d5a710e7641e0beb5e401ee9db7b90
Krzysztof Czarnecki <krzysztof.czarnecki@technica-engineering.de>
2022-03-23 10:53:07 +0100

    feat: `UnknownSource` exception

--- resourcerer/exceptions.py
+++ resourcerer/exceptions.py
@@ -1,3 +1,8 @@
 class UnknownCachingStrategy(Exception):
     """Raised when an unknown caching strategy is used"""
     pass
+
+
+class UknownSource(Exception):
+    """Raised when the source has no implemented driver"""
+    pass

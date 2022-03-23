df85fa29253c19d854e84616b66f72b7c5484327
Krzysztof Czarnecki <krzysztof.czarnecki@technica-engineering.de>
2022-03-23 10:54:26 +0100

    doc: YAML parser

--- resourcerer/parse_yaml.py
+++ resourcerer/parse_yaml.py
@@ -3,6 +3,7 @@ import yaml
 
 
 def get_yaml_obj(path: Path):
+    """Loads a YAML file as a dictionary."""
     with open(path, "r") as f:
         resources_obj = yaml.full_load(f)
     return resources_obj

822fde1b5f546f6da43b0593bee677a27652d772
Krzysztof Czarnecki <krzysztof.czarnecki@technica-engineering.de>
2022-03-23 10:54:53 +0100

    feat: aliases for the types used in the `_conf` file

--- resourcerer/types.py
+++ resourcerer/types.py
@@ -1,4 +1,13 @@
-from typing import Callable
+from typing import Callable, Optional, Tuple
 from pathlib import Path
 
 CacheResolverFunc = Callable[[str, Path], bool]
+SourcePath = Path
+TargetPath = Optional[Path]
+UploadSourcePath = Path
+UploadTargetPath = Path
+CheckCacheFirst = bool
+CachingStrategy = str
+SenderFunc = Callable[[SourcePath, TargetPath, CheckCacheFirst, CachingStrategy], None]
+DownloaderFunc = Callable[[UploadSourcePath, UploadTargetPath], None]
+SourceInterfaces = Tuple[DownloaderFunc, SenderFunc]
